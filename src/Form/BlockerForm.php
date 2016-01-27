9ab0d0bccff597f0caff52ae6a9482a8463a1e2e
Ted Bowman <ted.git@tedbow.com>
2016-01-27 13:48:31 -0500

    Starting Point for class

---
+++ src/Form/BlockerForm.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\user_blocker\Form\BlockerForm.
+ */
+
+namespace Drupal\user_blocker\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class BlockerForm.
+ *
+ * @package Drupal\user_blocker\Form
+ */
+class BlockerForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'user_blocker_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['username'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Username'),
+      '#description' => $this->t('Enter the username of the user you want to block.'),
+      '#maxlength' => 64,
+      '#size' => 64,
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#title' => $this->t('Block User'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+}
